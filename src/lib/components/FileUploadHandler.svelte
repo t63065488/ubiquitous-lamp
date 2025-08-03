a5a22f29c6e8013df9f3bcac56f35da0c29629d1
t63065488 <t63065488@gmail.com>
2025-08-03 21:14:12 +0000

    Initial file upload component wrapper

---
+++ src/lib/components/FileUploadHandler.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import { FileUpload } from '@skeletonlabs/skeleton-svelte';
+	import { FilePlus, File, CircleX } from '@lucide/svelte';
+
+	interface FileUploadHandlerProps {
+		name: string;
+		onFileChange: () => void;
+		onFileRejection: () => void;
+	}
+
+	const { name, onFileChange: onUpload }: FileUploadHandlerProps = $props();
+</script>
+
+<FileUpload {name} onFileChange={onUpload}>
+	{#snippet iconInterface()}
+		<FilePlus class="size-4" />
+	{/snippet}
+	{#snippet iconFile()}
+		<File class="size-4" />
+	{/snippet}
+	{#snippet iconFileRemove()}
+		<CircleX class="size-4" />
+	{/snippet}
+</FileUpload>
--- src/routes/count/+page.svelte
+++ src/routes/count/+page.svelte
@@ -1,1 +1,9 @@
-<p>Counts route</p>
+<script>
+	import FileUploadHandler from '$lib/components/FileUploadHandler.svelte';
+</script>
+
+<FileUploadHandler
+	name="Count"
+	onFileChange={() => console.log('test')}
+	onFileRejection={() => console.log('Test')}
+/>
