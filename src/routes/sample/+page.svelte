90ce7f6673853fd3209ef6591f02253bd4b02a6f
t63065488 <t63065488@gmail.com>
2025-08-04 16:44:01 +0000

    Add percentage slider to sample

--- src/routes/sample/+page.svelte
+++ src/routes/sample/+page.svelte
@@ -1,5 +1,8 @@
 <script lang="ts">
 	import FileUploadHandler from '$lib/components/FileUploadHandler.svelte';
+    import { Slider } from '@skeletonlabs/skeleton-svelte';
+
+    let value: number[] = $state([10])
 
 	const processFile = async (file: File) => {
 		console.log(file);
@@ -11,7 +14,9 @@
 	<h2 class="pb-4 h2">Sample Data</h2>
 	<FileUploadHandler name="Count" {processFile}>
 		{#snippet additions()}
-			<p>Test</p>
+            <p>Select Sample Percentage</p>
+			<Slider name="sample-percentage-input" {value} onValueChange={(e) => (value = e.value)} markers={[10, 25, 50, 75, 90]}/>
+            <p class="p opacity-60">Current: {value[0]}%</p>
 		{/snippet}
 	</FileUploadHandler>
 </div>
